153d0dd1314de0eda46a11a72829c30c6ad4cd4e
karas1910 <onenight1910@gmail.com>
2020-06-29 20:16:28 +0900

    add Splash Activity

---
+++ app/src/main/java/com/example/splash_sample/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.splash_sample
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onDestroy() {
+        Log.d("hoge", "Destroy")
+        super.onDestroy()
+    }
+}
